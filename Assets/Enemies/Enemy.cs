91c11488a228ee3dd8374a645a177d7e60b51aaa
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-08-10 01:33:09 -0400

    Make enemies pathfind to player

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.Characters.ThirdPerson;
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 4f;
 
     float currentHealthPoints = 100f;
 
-	public float healthAsPercentage {
+    AICharacterControl aiCharacterControl = null;
+    GameObject player = null;
+
+
+    public float healthAsPercentage {
         get {
             return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    void Start () {
+        aiCharacterControl = GetComponent<AICharacterControl> ();
+        player = GameObject.FindGameObjectWithTag ("Player");
+    }
+
+    void Update () {
+        float distanceToPlayer = Vector3.Distance (player.transform.position, transform.position);
+
+        if (distanceToPlayer <= attackRadius) {
+            aiCharacterControl.SetTarget (player.transform);
+        } else {
+            aiCharacterControl.SetTarget (transform);
         }
     }
 
