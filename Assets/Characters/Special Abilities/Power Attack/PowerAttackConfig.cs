93f54f35092038aca4e5803035070811acc002be
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-08-26 02:14:34 -0400

    Storing special ability config data

---
+++ Assets/Characters/Special Abilities/Power Attack/PowerAttackConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters {
+    [CreateAssetMenu(menuName = ("RPG/Special Ability/Power Attack"))]
+    public class PowerAttackConfig : SpecialAbilityConfig {
+
+        [Header("Power Attack Specific")]
+        [SerializeField] float extraDamage = 10f;
+
+        public override ISpecialAbility AddComponent(GameObject gameObjectToAttachTo) {
+            var behaviourComponent = gameObjectToAttachTo.AddComponent<PowerAttackBehaviour>();
+            behaviourComponent.SetConfig(this);
+            return behaviourComponent;
+        }
+
+    } // PowerAttackConfig
+}
