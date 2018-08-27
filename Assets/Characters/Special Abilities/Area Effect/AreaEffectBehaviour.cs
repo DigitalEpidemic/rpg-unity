497b5d07043206b0e9d9534524432b2c3eb91dc0
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-08-27 07:44:52 -0400

    Create an area of effect ability

---
+++ Assets/Characters/Special Abilities/Area Effect/AreaEffectBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+
+namespace RPG.Characters {
+    public class AreaEffectBehaviour : MonoBehaviour, ISpecialAbility {
+
+        AreaEffectConfig config;
+
+        public void SetConfig(AreaEffectConfig configToSet) {
+            this.config = configToSet;
+        }
+
+        void Start() {
+            print("AreaEffectBehaviour attached to: " + gameObject.name);
+        }
+
+        public void Use(AbilityUseParams useParams) {
+            print("AreaEffectBehaviour used by: " + gameObject.name);
+
+            // Static sphere cast for target
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, config.GetRadius(), Vector3.up, config.GetRadius());
+
+            foreach (RaycastHit hit in hits) {
+                var damageable = hit.collider.gameObject.GetComponent<IDamageable>();
+
+                if (damageable != null) {
+                    float damageToDeal = useParams.baseDamage + config.GetDamageToEachTarget(); // TODO Is this okay?
+                    damageable.TakeDamage(damageToDeal);
+                }
+            }
+        }
+
+    } // AreaEffectBehaviour
+}
---
+++ Assets/Characters/Special Abilities/Area Effect/AreaEffectConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters {
+    [CreateAssetMenu(menuName = ("RPG/Special Ability/Area Effect"))]
+    public class AreaEffectConfig : SpecialAbility {
+
+        [Header("Area Effect Specific")]
+        [SerializeField] float radius = 5f;
+        [SerializeField] float damageToEachTarget = 15f;
+
+        public override void AttachComponentTo(GameObject gameObjectToAttachTo) {
+            var behaviourComponent = gameObjectToAttachTo.AddComponent<AreaEffectBehaviour>();
+            behaviourComponent.SetConfig(this);
+            behaviour = behaviourComponent;
+        }
+
+        public float GetDamageToEachTarget() {
+            return damageToEachTarget;
+        }
+
+        public float GetRadius() {
+            return radius;
+        }
+
+    } // PowerAttackConfig
+}
