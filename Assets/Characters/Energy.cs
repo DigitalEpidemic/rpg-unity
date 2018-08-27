dba338ea1eed1e61e3b6031f0b4747bdefab44cc
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-08-27 11:13:34 -0400

    Using image fill in UI

--- Assets/Characters/Energy.cs
+++ Assets/Characters/Energy.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 namespace RPG.Characters {
     public class Energy : MonoBehaviour {
 
-        [SerializeField] RawImage energyBar = null;
+        [SerializeField] Image energyOrb = null;
         [SerializeField] float maxEnergyPoints = 100f;
         [SerializeField] float regenPointsPerSecond = 1f;
 
@@ -15,6 +15,7 @@ namespace RPG.Characters {
 
         void Start() {
             currentEnergyPoints = maxEnergyPoints;
+            UpdateEnergyBar();
         }
 
         void Update() {
@@ -40,9 +41,7 @@ namespace RPG.Characters {
         }
 
         private void UpdateEnergyBar() {
-            // TODO Remove magic numbers
-            float xValue = -(EnergyAsPercent() / 2f) - 0.5f;
-            energyBar.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+            energyOrb.fillAmount = EnergyAsPercent();
         }
 
         float EnergyAsPercent() {
