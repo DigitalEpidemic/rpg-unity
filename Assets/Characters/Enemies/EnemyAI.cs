3bb3699b9dcce33a0ad414404ba3204794146765
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-09-04 03:02:02 -0400

    Patrolling using coroutines

--- Assets/Characters/Enemies/EnemyAI.cs
+++ Assets/Characters/Enemies/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +9,15 @@ namespace RPG.Characters {
     public class EnemyAI : MonoBehaviour {
 
         [SerializeField] float chaseRadius = 6f;
+        [SerializeField] WaypointContainer patrolPath;
+        [SerializeField] float waypointTolerance = 2f;
 
         PlayerMovement player;
         Character character;
 
         float currentWeaponRange;
         float distanceToPlayer;
+        int nextWaypointIndex;
 
         enum State { idle, patrolling, attacking, chasing }
         State state = State.idle;
@@ -30,7 +34,7 @@ namespace RPG.Characters {
 
             if (distanceToPlayer > chaseRadius && state != State.patrolling) {
                 StopAllCoroutines();
-                state = State.patrolling;
+                StartCoroutine(Patrol());
             }
 
             if (distanceToPlayer <= chaseRadius && state != State.chasing) {
@@ -41,6 +45,23 @@ namespace RPG.Characters {
             if (distanceToPlayer <= currentWeaponRange && state != State.attacking) {
                 StopAllCoroutines();
                 state = State.attacking;
+            }
+        }
+
+        IEnumerator Patrol() {
+            state = State.patrolling;
+
+            while (true) {
+                Vector3 nextWayPointPos = patrolPath.transform.GetChild(nextWaypointIndex).position;
+                character.SetDestination(nextWayPointPos);
+                CycleWaypointWhenClose(nextWayPointPos);
+                yield return new WaitForSeconds(0.5f); // TODO Parameterize
+            }
+        }
+
+        void CycleWaypointWhenClose(Vector3 nextWayPointPos) {
+            if (Vector3.Distance(transform.position, nextWayPointPos) <= waypointTolerance) {
+                nextWaypointIndex = (nextWaypointIndex + 1) % patrolPath.transform.childCount;
             }
         }
 
