11aeb1a8273d711e6eb6577e9576a0d9d501548b
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-09-06 06:18:44 -0400

    Fixed GUIDs/AudioTrigger and added an enemy (Older enemys are broken)

--- Assets/Core/AudioTrigger.cs
+++ Assets/Core/AudioTrigger.cs
@@ -3,26 +3,25 @@
 public class AudioTrigger : MonoBehaviour {
 
     [SerializeField] AudioClip clip;
-    [SerializeField] int layerFilter = 0;
-    [SerializeField] float triggerRadius = 5f;
+    [SerializeField] int layerFilter = 11;
+    [SerializeField] float playerDistanceThreshold = 5f;
     [SerializeField] bool isOneTimeOnly = true;
 
-    [SerializeField] bool hasPlayed = false;
+    bool hasPlayed = false;
     AudioSource audioSource;
+    GameObject player; // will only trigger on distance to player
 
     void Start() {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.playOnAwake = false;
         audioSource.clip = clip;
 
-        SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
-        sphereCollider.isTrigger = true;
-        sphereCollider.radius = triggerRadius;
-        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+        player = GameObject.FindWithTag("Player");
     }
 
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.layer == layerFilter) {
+    void Update() {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer <= playerDistanceThreshold) {
             RequestPlayAudioClip();
         }
     }
@@ -38,7 +37,7 @@ public class AudioTrigger : MonoBehaviour {
 
     void OnDrawGizmos() {
         Gizmos.color = new Color(0, 255f, 0, .5f);
-        Gizmos.DrawWireSphere(transform.position, triggerRadius);
+        Gizmos.DrawWireSphere(transform.position, playerDistanceThreshold);
     }
 
 } // AudioTrigger
